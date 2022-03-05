b2c36cb29177a785c02e924c3806a8593e56811c
wt12318 <1650464505@qq.com>
2022-03-05 15:29:00 +0800

    update rpep for extracting random peptides from protein sequence

--- R/rpep.R
+++ R/rpep.R
@@ -1,3 +1,30 @@
-rpep <- function(num){
+#' Get random peptides from protein sequences
+#'
+#' @param dt A dataframe with at least 1 column containg protein sequence
+#' @param mer_len An integer number, the length of random peptide
+#' @param random_num An integer number, the number of random peptides needed
+#'
+#' @return A dataframe with a column named random_pep, containg the random peptides of sepecific length extracted from given proteins.
+#' @export
+#'
+#' @examples
+#'res <- rpep(uniprot_protein_test,mer_len=9,random_num=100)
+#'
+#'
+rpep <-  function(dt,mer_len,random_num){
+  dt$len <- nchar(dt$seq)
+  dt <- dt %>% filter(len >= mer_len)
+  dt$max_num <- dt$len - mer_len + 1
+  all_num <- cumsum(dt$max_num)
+  ##random select peptides
+  select_num <- sample(1:all_num[length(all_num)],size = random_num)
+  select_pep <- findInterval(select_num,all_num) + 1
+  ids_freq <- table(select_pep) %>% as.data.frame()##which protein is selected and how many random peptides need from each protein
 
+  res <- mapply(get_random_pep,
+                id = ids_freq$select_pep,
+                num = ids_freq$Freq,MoreArgs = list(dt=dt,mer_len=mer_len),SIMPLIFY = FALSE)
+  res <- bind_rows(res)
+  colnames(res) <- "random_pep"
+  return(res)
 }
